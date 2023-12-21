e52f43f941085af3dcab5efc18ad5e803755c0c2
tkmcnierney <tkmcnierney@berkeley.edu>
2023-12-20 23:52:18 -0800

    move edu beneath first 3 cards

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8a71a2502eaf4d4ff39efc707e6d2723",
-"index.html": "cec420fb73435193d80e0cf64d5c3664",
-"/": "cec420fb73435193d80e0cf64d5c3664",
-"main.dart.js": "734531d8d4a558cebf8c3cc33b2497cc",
+"index.html": "50ccc6ecbf0b0d064d389639ae47f9a6",
+"/": "50ccc6ecbf0b0d064d389639ae47f9a6",
+"main.dart.js": "17a497f7cdaf7d5eee9abcf49082fcd1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
