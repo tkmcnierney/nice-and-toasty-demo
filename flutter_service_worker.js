883ce606161872a66cd4e75489dcefeb62d3b530
tkmcnierney <tkmcnierney@berkeley.edu>
2024-02-29 12:02:02 -0800

    update card format

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8a71a2502eaf4d4ff39efc707e6d2723",
-"index.html": "8459cf6664a6c3037e1654e277bd03a2",
-"/": "8459cf6664a6c3037e1654e277bd03a2",
-"main.dart.js": "8370a91c3226a51e95f026af3b33c7c6",
+"index.html": "ec20a70909a6e777e344e7a0f2ff3501",
+"/": "ec20a70909a6e777e344e7a0f2ff3501",
+"main.dart.js": "55c1ad5cae00538109bf790b9962b000",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
